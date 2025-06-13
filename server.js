ffe3cd2dd3baf0a8d76775de932ddc3a2e7f5577
puncharat555 <posh34posh@gmail.com>
2025-06-13 20:43:55 +0700

    first commit

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const uri = process.env.MONGO_URI;
+
 // ใช้ CORS และ JSON middleware
 app.use(cors());
 app.use(express.json());
