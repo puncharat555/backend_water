6e08d12e8caff12019ad51006a7ac9cb63e5ef65
puncharat555 <posh34posh@gmail.com>
2025-06-13 21:05:01 +0700

    first commit

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on port ${port}`);
+});
+
+
 const uri = process.env.MONGO_URI;
 
 // ใช้ CORS และ JSON middleware
